7ca435113af76186460b8885e63136dbcc6eaa67
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-04-06 00:23:30 +0100

    overide the font family

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Contact from "./pages/Contact";
 import Homepage from "./pages/Homepage";
 import Innerpage from "./pages/Innerpage";
 import Layout from "./pages/Layout";
+import { createTheme, ThemeProvider} from "@mui/material"
+
+const theme = createTheme({
+  typography: {
+    fontFamily:[
+      "Montserrat"
+    ]
+  }
+})
 function App() {
   return (
     <Router>
-      <>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/innerpage" element={<Innerpage/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-          </Routes>
-        </Layout>
-      </>
+      <ThemeProvider theme={theme}>
+        <>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Homepage/>}/>
+              <Route path="/innerpage" element={<Innerpage/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+          </Layout>
+        </>
+      </ThemeProvider>
     </Router>
   );
 }
